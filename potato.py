4ec38d7905906d3fa8dc1410cce00809ed96053f
Scholar <scholar@paradigm.local>
2023-09-05 13:30:25 -0600

    created variables and functuns

---
+++ potato.py
@@ -0,0 +1,25 @@
+person =potato
+
+list = (one, two, three)
+    
+dictionary = (
+    'name': 'potato'
+    "age": 16
+)
+   
+def is_even_number(number):
+    if number % 2 == 0:
+       print('this is an even number')
+    else:
+       print('this is an odd number')
+
+is_even_number(2)
+
+MAIN_CHARACTER = 'mario'
+
+main_character = 'luigi'
+
+MyNameIsNot = 'cheese'
+
+my_name = 'bob'
+
---
+++ variables.py
@@ -0,0 +1,21 @@
+
+Name = 'oliver'
+Best_Test_Score = 100
+Lunch_Was_Tasty = False
+List_Of_B_Day_Classes = ['Gym', 'Math', 'Computer Programing', 'Art Meditations']
+
+print(f'Hello, My Name Is {Name}. Today I Got A test Score Of {Best_Test_Score}. In A Unknown Class. My Classes Today Are {List_Of_B_Day_Classes}.')
+
+def get_num_4():
+    print(2+2)
+
+def add_two_numbers(num1, num2):
+    print(num1+num2)
+
+add_two_numbers(89,46)
+
+user_number1 = int(input('Give me a number:'))
+user_number2 = int(input('Give me a 2nd number:'))
+
+def add_two_numbers2(user_number1, user_number2):
+    print(user_number1+user_number2)
