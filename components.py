b81267af20821e3a0a1c94b20c9ade0053230ac0
Scholar <scholar@paradigm.local>
2023-09-08 11:26:08 -0600

    intro to concepts

---
+++ components.py
@@ -0,0 +1,21 @@
+#PEMDAS
+
+#y = 10
+#x = 10
+#x = y >= 10
+
+#z = 7 // 2
+#t = 7/ 2
+#print(z)
+
+#def my_function():
+#    print('I am so great!')
+
+#my_function()
+
+#def add(num1,num2):
+#    print(num1+num2)
+
+my_name = 'Mr Oliver'
+words = my_name.split(' ')
+print(words)
