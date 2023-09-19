fce7248ee688bd0858bacbfe4a3004347d2331fd
Scholar <scholar@paradigm.local>
2023-09-19 13:34:00 -0600

    intro to control

---
+++ Intro_to_Control_Structures.py
@@ -0,0 +1,32 @@
+# #statements
+# car = "yellow"
+# car_made = 'cheese'
+
+# if car == 'blue' or car == 'Blue':
+#     if car_made == 'cheese':
+#         print('my car is made of blue cheese')
+#     else:
+#         print('my car is blue')
+# elif car == 'yellow' or car == 'Yellow':
+#     print('my car is made of yellow cheese')
+# else:
+#     print('me have no cheese')
+
+
+# #forloop
+# for i in range(1, 100001):
+#     print(i)
+
+
+# sports = ['soccer', 'basketball', 'hockey', 'football']
+
+# for item in sports:
+#     print(item)
+
+
+#while loop
+
+# i = 1
+# while i < 11:
+#     print('Cheesse')
+#     i += 1
