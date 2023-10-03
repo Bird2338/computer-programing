e055ca9fb46f607de60ea3c4d42b14735fb5e02f
Scholar <scholar@paradigm.local>
2023-10-03 13:35:37 -0600

    loops

--- For_Loops.py
+++ For_Loops.py
@@ -1,23 +1,23 @@
-# word = 'cheese'
-# for item in word:
-#     print(item)
+word = 'cheese'
+for item in word:
+    print(item)
 
 
-# for nums_1 in range(1, 10):
-#     print(nums)
+for nums_1 in range(1, 10):
+    print(nums_1)
 
 
-# Cheese_List = ['Bree', 'Parm', 'Goat', 'Blue']
-# print('Cheese types:')
-# for Cheese in Cheese_List:
-#     print(Cheese)
+Cheese_List = ['Bree', 'Parm', 'Goat', 'Blue']
+print('Cheese Types:')
+for Cheese in Cheese_List:
+    print(Cheese)
 
-
-# flowers = ['Jasmine','Lotus','Rose','Sunflower']
-# for Fl in flowers:
-#     if Fl == 'Rose':
-#          break
-#     else:
-#          print(Fl)
+print("Flower Types:")
+flowers = ['Jasmine','Lotus','Rose','Sunflower']
+for Fl in flowers:
+    if Fl == 'Rose':
+         break
+    else:
+         print(Fl)
 
 
