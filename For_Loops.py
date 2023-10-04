983a910e1ee256a7fd877aa76e44467dacf05567
oliver <143126665+Birb2338@users.noreply.github.com>
2023-10-03 18:41:11 -0600

    Update For_Loops.py

--- For_Loops.py
+++ For_Loops.py
@@ -1,23 +1,59 @@
-# word = 'cheese'
-# for item in word:
-#     print(item)
+word = 'cheese'
+for item in word:
+    print(item)
 
 
-# for nums_1 in range(1, 10):
-#     print(nums_1)
+for nums_1 in range(1, 10):
+    print(nums_1)
 
 
-# Cheese_List = ['Bree', 'Parm', 'Goat', 'Blue']
-# print('Cheese types:')
-# for Cheese in Cheese_List:
-#     print(Cheese)
+Cheese_List = ['Bree', 'Parm', 'Goat', 'Blue']
+print('Cheese types:')
+for Cheese in Cheese_List:
+    print(Cheese)
+    
+    
+flowers = ['Jasmine','Lotus','Rose','Sunflower']
+for Fl in flowers:
+    if Fl == 'Rose':
+         break
+    else:
+         print(Fl)
+
+list1 = [5,10,15,20]
+list2 = ['Tomatoes','Potatoes','Carrots','Cucumbers']
+for nums_2 in list1:
+    for veggies in list2:
+        print(f'{nums_2} {veggies}')
+        
+        
+colors = ['blue','red','green','yellow']
+for co in colors:
+    if co == 'green':
+         break
+    else:
+         print(co)
 
 
-# flowers = ['Jasmine','Lotus','Rose','Sunflower']
-# for Fl in flowers:
-#     if Fl == 'Rose':
-#          break
-#     else:
-#          print(Fl)
+vehicles = ['Car','Cycle','Bus','Tempo']
+for ve in vehicles:
+    if ve == 'Bus':
+         continue
+    else:
+         print(ve)
+         
+         
+Numbers = [12,3,56,67,89,90]
+count = 0
+for nums_4 in Numbers:
+    count = count + 1
+print(count)
 
 
+Numbers = [12,3,56,67,89,90]
+Sum = 0
+for nums_3 in Numbers:
+    Sum = (Sum + nums_3)
+    print(Sum)
+    
+    
