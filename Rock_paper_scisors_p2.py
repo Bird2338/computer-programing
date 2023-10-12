82a057ed9b9e2bf1b484f478df0ca0948e563d37
Scholar <scholar@paradigm.local>
2023-10-12 13:33:23 -0600

    rock

--- Rock_paper_scisors_p2.py
+++ Rock_paper_scisors_p2.py
@@ -10,15 +10,21 @@ elif p1_input == 'paper' or p1_input == 'Paper':
     p1_input = 2
 elif p1_input == 'scissors' or p1_input == 'Scissors':
     p1_input = 3
-
+else:
+    print('ERROR')
+    
 if p2_input == 'rock' or p2_input == 'Rock':
     p2_input = 1
 elif p2_input == 'paper' or p2_input == 'Paper':
     p2_input = 2
 elif p2_input == 'scissors' or p2_input == 'Scissors':
     p2_input = 3
+else:
+    print('ERROR')
 
 if (p1_input > p2_input) or (p1_input == 1 and p2_input == 3):
     print('Player One Wins')
-else:
+elif (p2_input > p1_input) or (p2_input == 1 and p1_input == 3):
     print('Player Two Wins')
+else:
+    print('ERROR')
