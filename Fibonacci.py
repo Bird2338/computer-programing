35995e0815880654f4df1325ec437ec63564b4dd
Scholar <scholar@paradigm.local>
2023-10-12 12:44:25 -0600

    fib

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,7 +1,8 @@
+x = int(input('How deep into the fibonacci do you want to go?'))
 y = 0
 total = 1
 last_total = 0
-for x in range(1, 10):
+for x in range(1, (x + 1)):
     y = total
     total = total + last_total
     last_total = y
---
+++ Rock_paper_scisors_p2.py
@@ -0,0 +1,24 @@
+p1_input = input('(Player One) Rock, Paper, scissors:')
+p2_input = input('(Player Two) Rock, Paper, scissors:')
+
+# if (p1_input == "Rock" or p1_input == "rock") and (p2_input == "Paper" or p2_input == "paper"):
+#     print('Player Two Wins')
+
+if p1_input == 'rock' or p1_input == 'Rock':
+    p1_input = 1
+elif p1_input == 'paper' or p1_input == 'Paper':
+    p1_input = 2
+elif p1_input == 'scissors' or p1_input == 'Scissors':
+    p1_input = 3
+
+if p2_input == 'rock' or p2_input == 'Rock':
+    p2_input = 1
+elif p2_input == 'paper' or p2_input == 'Paper':
+    p2_input = 2
+elif p2_input == 'scissors' or p2_input == 'Scissors':
+    p2_input = 3
+
+if (p1_input > p2_input) or (p1_input == 1 and p2_input == 3):
+    print('Player One Wins')
+else:
+    print('Player Two Wins')
