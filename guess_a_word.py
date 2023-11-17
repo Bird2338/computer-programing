621e36bafb450da19bdaa83e704f365d724c5688
Scholar <scholar@paradigm.local>
2023-11-17 11:30:53 -0700

    cheese

--- guess_a_word.py
+++ guess_a_word.py
@@ -1,15 +1,5 @@
-indices = {1}
-out = [int(i in indices) for i in range(10)]
-print(out)
 #Random Word Genorator
 #random_word Output is Lower Case
-myList = ['one', 'two', 'three']
-
-myList.append('four')
-
-print(myList)
-
-# ['one', 'two', 'three', 'four']
 y = 1
 import random
 x = (random.randint(1, 267751))
@@ -21,18 +11,19 @@ with open('words.txt') as f:
   random_word = f.readline().lower()
 
 print(random_word)
+print(len(random_word))
 
 
 #User Guesses A word
 #user_guess Output Is Lower Case
-
-
 is_right = False
 while is_right == False:
-  user_input = input('What Is Your Guess').lower()
-  if len(random_word) != len(user_input):
+  user_input = input('What Is Your Guess: ').lower()
+  if len(random_word) - 1 != len(user_input):
     print('Wrong')
   else:
-    for z in range(len(random_word)):
-      for q in range(len(user_input)):
-        break
+    for z in range(len(random_word) - 1):
+      print(user_input[z])
+      print(random_word[z])
+      if user_input[z] == random_word[z]:
+        print('yay')
