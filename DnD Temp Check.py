1f336de6dcd2c86ca514ef51ad92ed3e55f1933e
oliver <143126665+Birb2338@users.noreply.github.com>
2023-11-28 14:55:00 -0700

    Update DnD Temp Check.py

--- DnD Temp Check.py
+++ DnD Temp Check.py
@@ -1,25 +1,58 @@
-x = 𝚿
-
-
-month_input = input('what month num is it? ')
+import random
+month_input = int(input('what month num is it? '))
 mountan_input = input('in mountans? y/n ').lower()
-latitude = input('what is the latitude? ')
-longitude = input('what is the longitude? ')
+latitude = int(input('what is the latitude? '))
+longitude = int(input('what is the longitude? '))
 
-month_input_check = month_input.isnumeric()
 mountan_input_check = mountan_input.isalpha()
-latitude_check = latitude.isnumeric()
-longitude_check = longitude.isnumeric()
 
 x = 0
-if (month_input_check == True) and (mountan_input_check == True) and (latitude_check == True) and (longitude_check == True):
-    if latitude or longitude <= 50:
-        if latitude or longitude >= -50:
-            if month_input <= 12:
-                if month_input >= 1:
-                    if mountan_input == 'y' or mountan_input == 'n':
-                        x = 1
-if x == 0:
-    print('ERROR')
-else:
-    print('temp')
+latitude = -51
+for i in range(100):
+    longitude = -51
+    x = latitude
+    latitude = x + 1
+    for h in range(100):
+        t = longitude
+        longitude = t + 1
+    if (mountan_input_check == True):
+        if latitude or longitude <= 50:
+            if latitude or longitude >= -50:
+                if month_input <= 12:
+                    if month_input >= 1:
+                        if mountan_input == 'y' or mountan_input == 'n':
+                            x = 1
+                            if month_input == 1:
+                                month_temp = -20 + random.randrange(-5,5)
+                            elif month_input == 2:
+                                month_temp = -10 + random.randrange(-5,5)
+                            elif month_input == 3:
+                                month_temp = 0 + random.randrange(-5,5)
+                            elif month_input == 4:
+                                month_temp = 0 + random.randrange(-5,5)
+                            elif month_input == 5:
+                                month_temp = 10 + random.randrange(-5,5)
+                            elif month_input == 6:
+                                month_temp = 20 + random.randrange(-5,5)
+                            elif month_input == 7:
+                                month_temp = 20 + random.randrange(-5,5)
+                            elif month_input == 8:
+                                month_temp = 10 + random.randrange(-5,5)
+                            elif month_input == 9:
+                                month_temp = 0 + random.randrange(-5,5)
+                            elif month_input == 10:
+                                month_temp = 0 + random.randrange(-5,5)
+                            elif month_input == 11:
+                                month_temp = -10 + random.randrange(-5,5)
+                            elif month_input == 12:
+                                month_temp = -20 + random.randrange(-5,5)
+                            if mountan_input == 'n':
+                                temp = 50 - latitude + longitude + month_temp
+        print(f'({longitude},{latitude})')
+                        
+        # if x == 0:
+        #     print('ERROR')
+        # else:
+        #     # print(temp)
+        #     if temp >= 50:
+        #         print(f'({longitude},{latitude})')
