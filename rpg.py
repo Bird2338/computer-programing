95a36514b9d0e324097dc144449a62cdceee6d92
oliver <143126665+Birb2338@users.noreply.github.com>
2023-12-01 11:40:56 -0700

    Update rpg.py

--- rpg.py
+++ rpg.py
@@ -6,7 +6,7 @@ import random
 
 
 
-# ----------Start up text----------
+# --------------------Start up text--------------------
 # Def type start loop
 def type_start():
     start_input = input().lower()
@@ -17,13 +17,14 @@ def type_start():
 
 # running start up text
 print("Welcome to Oliver's magnifacint RPG simulator")
-time.sleep(2)
+time.sleep(1.5)
 print("Type start to play: ")
 type_start()
 
 
 
-# ----------Picking a name----------
+# --------------------Picking a name--------------------
+print(".")
 print("Welcome adventurer")
 time.sleep(1)
 print("Who will you be known as?: ")
@@ -31,13 +32,123 @@ player_name = input()
 
 
 
-# ----------Picking a race----------
+# --------------------Picking a race--------------------
+# def race stat view back loop
+def race_stat_view():
+    race_stat_view_input = input().lower()
+    if race_stat_view_input == "back":
+        race_info_menu()
+    else:
+        race_stat_view()
+    
+#  def race info input
+def race_info_menu_input():
+    race_info_input = input().lower()
+    if race_info_input == "back":
+        ()
+    elif race_info_input == "human":
+        print(".")
+        print("- +1 to every stat")
+        time.sleep(0.5)
+        print("type 'back' to go back")
+        race_stat_view()
+    elif race_info_input == "dwarf":
+        print(".")
+        print("- +2 to Constitution")
+        time.sleep(0.5)
+        print("type 'back' to go back")
+        race_stat_view()
+    elif race_info_input == "gnome":
+        print(".")
+        print("- +2 to Intelligence")
+        time.sleep(0.5)
+        print("type 'back' to go back")
+        race_stat_view()
+    elif race_info_input == "goblin":
+        print(".")
+        print("- +2 to Dexterity and +1 to Constitution")
+        time.sleep(0.5)
+        print("type 'back' to go back")
+        race_stat_view()
+    elif race_info_input == "halfling":
+        print(".")
+        print("- +2 to Dexterity")
+        time.sleep(0.5)
+        print("type 'back' to go back")
+        race_stat_view()
+    elif race_info_input == "elf":
+        print(".")
+        print("- +2 to Dexterity")
+        time.sleep(0.5)
+        print("type 'back' to go back")
+        race_stat_view()
+    elif race_info_input == "half-elf":
+        print(".")
+        print("- +2 to Charisma and 2 Ability Score increases")
+        time.sleep(0.5)
+        print("type 'back' to go back")
+        race_stat_view()
+    elif race_info_input == "tiefling":
+        print(".")
+        print("- +2 to Charisma and +1 to Intelligence")
+        time.sleep(0.5)
+        print("type 'back' to go back")
+        race_stat_view()
+    else:
+        print("invalid input")
+        race_info_menu_input()
+        
+# def race info menu
+def race_info_menu():
+    print(".")
+    print("Each race efects your stats")
+    time.sleep(0.5)
+    print("Races:")
+    time.sleep(0.5)
+    print("Human")
+    time.sleep(0.5)
+    print("Dwarf")
+    time.sleep(0.5)
+    print("Gnome")
+    time.sleep(0.5)
+    print("Goblin")
+    time.sleep(0.5)
+    print("Halfling")
+    time.sleep(0.5)
+    print("Elf")
+    time.sleep(0.5)
+    print("Half-Elf")
+    time.sleep(0.5)
+    print("Tiefling")
+    time.sleep(0.5)
+    print("Type a 'race' to see it's stats or type 'back'")
+    race_info_menu_input()
+    
 # race picking while loop
 while race_picked == False:
-    time.sleep(1)
+    time.sleep(0.5)
+    print(".")
     print(f"What will {player_name}'s race be?")
-    time.sleep(1)
-    print("Type a 'race' or type 'race info': ")
+    time.sleep(0.5)
+    print("type 'race info' for information on races, or type 'rand' for a random race.")
     race_pick_input = input().lower()
     if race_pick_input == "race info":
-        print(True)
+        race_info_menu()
+    elif race_pick_input == "rand":
+        rand_race = random.randrange(1,9)
+        if rand_race == 1:
+            player_race = "Human"
+        elif rand_race == 2:
+            player_race = "Dwarf"
+        elif rand_race == 3:
+            player_race = "Gnome"
+        elif rand_race == 4:
+            player_race = "Goblin"
+        elif rand_race == 5:
+            player_race = "Halfling"
+        elif rand_race == 6:
+            player_race = "Elf"
+        elif rand_race == 7:
+            player_race = "Half-Elf"
+        elif rand_race == 8:
+            player_race = "Tiefling"
