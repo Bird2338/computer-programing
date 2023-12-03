c2147baac6d1f73f45f0709d076a55b0f62c61c5
oliver <143126665+Birb2338@users.noreply.github.com>
2023-12-02 21:17:15 -0700

    Update RPG_Revised.py

--- RPG_Revised.py
+++ RPG_Revised.py
@@ -1,1 +1,36 @@
+# Inports and basic variables
+import time
+import random
+
+# --------------------Start up text--------------------
+# Def type start loop
+def type_start():
+    start_input = input().lower()
+    if start_input == "start":
+        ()
+    else:
+        type_start()
+
+# running start up text
+print("Welcome to Oliver's magnifacint RPG simulator")
+time.sleep(1)
+print("Type start to play: ")
+type_start()
+
+
+
+# --------------------Picking a name--------------------
+
+
+
+# --------------------Picking a race--------------------
+
+
+
+# --------------------Picking stats--------------------
+
+
+
+# --------------------Race stat increases--------------------
+
 
